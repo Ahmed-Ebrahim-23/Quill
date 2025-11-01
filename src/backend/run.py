e6c6e70befe9df1a835006667828a7e2ca34a28c
Ahmed-Ebrahim-23 <ahmed2322004ahmed@gmail.com>
2025-11-01 18:13:09 +0200

    build: setup Flask backend structure and database configration

---
+++ src/backend/run.py
@@ -0,0 +1,12 @@
+import os
+from settings.app import create_app
+
+if __name__ == "__main__":
+    config_name = os.getenv("FLASK_ENV")
+
+    app = create_app(config_name)
+
+    host = os.getenv("HOST")
+    port = int(os.getenv("PORT"))
+
+    app.run(host=host, port=port, debug=app.config["DEBUG"])
---
+++ src/backend/settings/config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config:
+    DEBUG = os.getenv("DEBUG").lower() in ("true", "1")
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JSON_SORT_KEYS = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+config = {
+    "default": DevelopmentConfig
+}
+
+def get_config(config_name=None):
+    return config.get(config_name, config["default"])
