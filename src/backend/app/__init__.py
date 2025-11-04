9c6d49da6fdda0f52cde964d9adb23903d73da87
Ahmed-Ebrahim-23 <ahmed2322004ahmed@gmail.com>
2025-11-04 22:56:44 +0200

    feat: add JWT for Authentication and CORS for frontend integration

--- src/backend/app/__init__.py
+++ src/backend/app/__init__.py
@@ -1,25 +1,29 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
+from flask_cors import CORS
 from config import get_config
 from .extensions import db, migrate 
+from .extensions import bcrypt, jwt
 from .routes import register_blueprints
 from .common import register_error_handlers
 
 load_dotenv()
 
 def create_app(config_name=None):
-    """
-    Application factory function.
-    """
     app = Flask(__name__)
     app.config.from_object(get_config(config_name))
 
     app.json.sort_keys = False
 
+    # Enable CORS  
+    CORS(app)
+
     # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
     
     # Register blueprints
     register_blueprints(app)
--- src/backend/config.py
+++ src/backend/config.py
@@ -9,6 +9,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES')) 
+
     JSON_SORT_KEYS = False
 
 class DevelopmentConfig(Config):
